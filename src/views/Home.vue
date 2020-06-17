8c357b8038a457044737a0a4e900c7d6d83b3868
sanjan274 <adhikari.sanjan@crowdbotics.com>
2020-06-17 23:20:55 +0800

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h1>HomePage</h1>
+        <p class="red white--text">Ex ad ut exercitation aliquip sunt officia. Voluptate exercitation nulla commodo id cillum nostrud esse nisi adipisicing cillum laborum Lorem amet proident. Ea officia in ex ex deserunt adipisicing cupidatat.</p>
+        <p class="pink lighten-1 red--text">Minim qui ad amet culpa. Lorem ullamco esse nisi nulla nostrud. Est veniam nostrud non non elit commodo. Minim dolor tempor officia dolor consectetur ex fugiat exercitation cupidatat duis minim. Magna eiusmod non ipsum non reprehenderit laboris id aliquip enim eiusmod eu eu dolor duis. Et dolore et reprehenderit magna veniam nulla aute.</p>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
